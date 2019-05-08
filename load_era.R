2ecadceafa0e2875317ca92e32d2c9da93e45dca
JM Delgado <jmigueldelgado@gmail.com>
2019-05-08 23:09:30 +0200

    loading python module

--- load_era.R
+++ load_era.R
@@ -1,2 +1,11 @@
+## this works:
+Sys.setenv(RETICULATE_PYTHON='/home/delgado/local/miniconda3/envs/ecmwf/bin/python')
 library(reticulate)
-use_virtualenv("ecmwf")
+py_config()
+ecmwf <- import('ecmwfapi')
+
+## does not work:
+library(reticulate)
+use_condaenv(condaenv = 'ecmwf', conda='/home/delgado/local/miniconda3/bin/conda')
+py_config()
+ecmwf <- import('ecmwfapi')
