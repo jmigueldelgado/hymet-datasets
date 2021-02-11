554e82b7aa68ea8d1e258c537df4c360b68ebc2c
JM Delgado <jmigueldelgado@posteo.net>
2021-02-11 15:09:01 +0100

    removes all old scripts. only python for era5 is now available

--- R/scraping-data.r
+++
@@ -1,9 +0,0 @@
-
-#' Variable names for reanalysis datasets.  Check ECMWF's parameter db in https://apps.ecmwf.int/codes/grib/param-db
-#'
-#' @docType data
-#' @usage data(vartable)
-#'
-#' @references none
-#' @source GPCC,NCEP,ERA
-"vartable"
