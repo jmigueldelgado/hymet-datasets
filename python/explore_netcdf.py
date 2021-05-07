6badf7918045db78edb75dd735c9f38c61566ef4
JM Delgado <jmigueldelgado@posteo.net>
2021-05-07 13:28:14 +0200

    improves paths

--- python/explore_netcdf.py
+++ python/explore_netcdf.py
@@ -11,7 +11,6 @@ import xarray as xr
 from urllib.request import urlopen
 from os import listdir
 from os import path
-
 # # Get the csv with peak location
 # df=pandas.read_csv('./data/peak_coordinates_revised3_WGS84.csv')
 # df_clean=df[(df.LON>-361) & (df.LAT>-91)]
@@ -19,8 +18,8 @@ from os import path
 # crs = {'init': 'epsg:4326'} #http://www.spatialreference.org/ref/epsg/2263/
 # geo_df = GeoDataFrame(df_clean, crs=crs, geometry=geometry)
 
-ncarray=listdir('./data/nc')
-
+ncdir=path.join(path.expanduser('~'),'proj','hymet-datasets','data','nc')
+ncarray=listdir(ncdir)
 def netcdf_open(ncpath):
     DS = xr.open_dataset(ncpath)
     return DS.assign(wind10m=(DS.u10**2+DS.v10**2)**0.5)
@@ -36,7 +35,7 @@ def daily_min(da):
 
 DSdailymean=list()
 for ncfile in ncarray:
-    DS=netcdf_open(path.join('data','nc',ncfile))
+    DS=netcdf_open(path.join(ncdir,ncfile))
     DSdailymean.append(daily_mean(DS))
 
-xr.concat(DSdailymean,'time').to_dataframe().to_csv('./data/out/daily_mean.csv')
+xr.concat(DSdailymean,'time').to_dataframe().to_csv(path.join(path.expanduser('~'),'proj','hymet-datasets','data','out','daily_mean.csv'))
